b3c4af0f23a784940d7447b1af011819af647405
Katsuhisa Maruyama <maru@cs.ritsumei.ac.jp>
2026-09-06 09:02:19 +0900

    Define annotations

---
+++ src/main/java/org/jtool/rujava/annotation/Immutable.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2026
+ *  Software Science and Technology Lab., Ritsumeikan University
+ */
+
+package org.jtool.rujava.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target({
+    ElementType.METHOD,
+    ElementType.CONSTRUCTOR,
+    ElementType.PARAMETER,
+    ElementType.LOCAL_VARIABLE,
+})
+@Retention(RetentionPolicy.SOURCE)
+public @interface Immutable {
+}
